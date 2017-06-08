2b097611b21981edf33a98a4525e92ddfc034546
sacha barber <sbarber@nephilaadvisors.co.uk>
2017-06-08 21:43:18 +0100

    navigation working YAY

--- PlayBackEndApi/FrontEndWebSite/src/index.tsx
+++ PlayBackEndApi/FrontEndWebSite/src/index.tsx
@@ -1,79 +1,79 @@
-import "reflect-metadata";
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import { Hello, HelloProps } from "./components/Hello";
-import { Foo } from "./domain/Foo";
-import { TYPES } from "./types";
-import { ContainerOperations } from "./ioc/ContainerOperations"; 
-import Rx from 'rx';  
+
+import 'bootstrap/dist/css/bootstrap.css';
+import {Nav, Navbar, NavItem, NavDropdown, MenuItem} from "react-bootstrap";
+
+import { Router, Route, hashHistory  } from 'react-router'
 
 
-(function () {
-
-    var evt;
-
-    window['clockChanged'] = function (incomingJsonPayload) {
-        evt = new CustomEvent('onClockChanged', { detail: incomingJsonPayload });
-        window.dispatchEvent(evt);
-    }
-
-    var source = Rx.Observable.fromEvent(window, 'onClockChanged');
-
-    var subscription = source.subscribe(
-        function (x) {
-            console.log('RX saw onClockChanged');
-            console.log('RX x = ', x.detail);
-        },
-        function (err) {
-            console.log('Error: %s', err);
-        },
-        function () {
-            console.log('Completed');
-        });
-
-} ());
+class MainNav extends React.Component<undefined, undefined> {
+  render() {
+    return (
+     <Navbar brand='React-Bootstrap'>
+         <Nav>
+             <NavItem eventKey={1} href='#/'>Home</NavItem>
+             <NavItem eventKey={2} href='#/contact'>Contact</NavItem>
+             <NavItem eventKey={2} href='#/about'>About</NavItem>
+         </Nav>
+     </Navbar>
+    )
+  }
+}
 
 
 
+class App extends React.Component<undefined, undefined> {
+  render() {
+    return (
 
-let foo = ContainerOperations.getInstance().container.get<Foo>(TYPES.Foo);
-
-let helloProps = {
-    compiler: "someCompilerXX",
-    framework: "someFramework",
-    foo: foo
-};
-
-let HelloComponent = React.createElement(Hello, helloProps, null)
-let HelloComponent2 = React.createElement(Hello, helloProps, null)
-
-
-var HelloHolder = React.createClass({
-    render: function () {
-        return (
-            <div>
-                <Hello compiler="TypeScript" framework="React" foo={foo} />
-                {HelloComponent}
-                {HelloComponent2}
-            </div>
-        )
-    }
-});
+        <div>
+            <MainNav/>
+            {this.props.children}
+        </div>
+    )
+  }
+}
 
 
+const Home = () => (
+  <div>
+    <h2>Home</h2>
+  </div>
+)
 
-ReactDOM.render(
-    //React.createElement(Hello, helloProps, null),
-    <div>
-        <HelloHolder/>
-    </div>,
-    document.getElementById('example')
-);
+const Contact = () => (
+  <div>
+    <h2>Contact</h2>
+  </div>
+)
+
+
+const About = () => (
+  <div>
+    <h2>About</h2>
+  </div>
+)
+
+
+
+ReactDOM.render((
+    <Router history={hashHistory}>
+            <Route component={App}>
+                <Route path="/" component={Home}/>
+                <Route path="/contact" component={Contact}/>
+                <Route path="/about" component={About}/>
+            </Route>
+        </Router>
+), document.getElementById('root'));
+
+
+
 
 
 //ReactDOM.render(
-//    <div id="div1">
-//        React.createElement(Hello, helloProps, null)
-//    </div>,
-//    document.getElementById("example")
+//    <MainNav/>,
+//    document.getElementById('root')
 //);
+
+
---
+++ PlayBackEndApi/public/dist/indexCss.bundle.a3395fbfe7aa0e324e69.js
@@ -0,0 +1,11 @@
+webpackJsonp([1],{
+
+/***/ 233:
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ })
+
+},[233]);
+//# sourceMappingURL=indexCss.bundle.a3395fbfe7aa0e324e69.js.map
